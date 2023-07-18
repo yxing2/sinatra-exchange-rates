27983f6796da6d2854776dfc3a87a6c70ca72205
yxing2 <136658270+yxing2@users.noreply.github.com>
2023-07-18 06:16:27 +0000

    second step done

--- app.rb
+++ app.rb
@@ -3,13 +3,20 @@ require "sinatra/reloader"
 require "http"
 require "json"
 
-get("/") do
-  currency_url = "https://api.exchangerate.host/symbols"
-  raw_response = HTTP.get(currency_url).to_s
-  parsed_response = JSON.parse(raw_response)
+currency_url = "https://api.exchangerate.host/symbols"
+raw_response = HTTP.get(currency_url).to_s
+parsed_response = JSON.parse(raw_response)
+results = parsed_response.fetch("symbols")
 
-  results = parsed_response.fetch("symbols")
+get("/") do
+  @currency = results.keys
+erb(:homepage)
+end
+
+get ("/:currency_url") do
+  @select_code = params.fetch("currency_url")
   @currency = results.keys
 
-erb(:homepage)
+
+erb(:flexible)
 end
