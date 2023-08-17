6eb261974f341ca39a4eb466772375c653d55a4e
yxing2 <136658270+yxing2@users.noreply.github.com>
2023-08-17 00:28:52 +0000

    Done

--- app.rb
+++ app.rb
@@ -3,24 +3,36 @@ require "sinatra/reloader"
 require "http"
 require "json"
 
+get("/") do
 currency_url = "https://api.exchangerate.host/symbols"
 raw_response = HTTP.get(currency_url).to_s
 parsed_response = JSON.parse(raw_response)
 results = parsed_response.fetch("symbols")
 
-get("/") do
   @currency = results.keys
 erb(:homepage)
 end
 
 get ("/:currency_one") do
+currency_url = "https://api.exchangerate.host/symbols"
+raw_response = HTTP.get(currency_url).to_s
+parsed_response = JSON.parse(raw_response)
+results = parsed_response.fetch("symbols")
+
   @code_one = params.fetch("currency_one")
   @currency = results.keys
 erb(:flexible)
 end
 
-get ("/:currency_one/currency_two") do
+get ("/:currency_one/:currency_two") do
+  @code_one = params.fetch("currency_one")
   @code_two = params.fetch("currency_two")
-  @currency = results.keys
+  
+  currency_url = "https://api.exchangerate.host/convert?from=#{@code_one}&to=#{@code_two}"
+  raw_response = HTTP.get(currency_url).to_s
+  parsed_response = JSON.parse(raw_response)
+  results = parsed_response.fetch("info")
+  @rate = results.fetch("rate")
+
 erb(:convert)
 end
